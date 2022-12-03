2d0b05f168f03271076a90403b8198e2573353df
CharleBlack96 <charlesblack@charless-mbp.lan>
2022-12-02 21:44:12 -0800

    added functions to helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -82,8 +82,8 @@ def addy_split(addy_series):
     for addy in addy_series:
         second_half = addy.split('\n')[1]
         city = second_half.split(',')[0]
-        state = second_half.split()[1]
-        zip = second_half.split()[2]
+        state = second_half.split()[-2]
+        zip = second_half.split()[-1]
 
         city_list.append(city)
         state_list.append(state)
@@ -96,17 +96,152 @@ def addy_split(addy_series):
     return df
 
 
-print(addy_split(address_df['address']))
+# print(addy_split(address_df['address']))
 
-asfluekh
+
 def abbr_2_st(state_series, abbr_2_st=True):
-    pass
+    state_dict = {
+            'AK': 'Alaska',
+            'AL': 'Alabama',
+            'AR': 'Arkansas',
+            'AS': 'American Samoa',
+            'AZ': 'Arizona',
+            'CA': 'California',
+            'CO': 'Colorado',
+            'CT': 'Connecticut',
+            'DC': 'District of Columbia',
+            'DE': 'Delaware',
+            'FL': 'Florida',
+            'GA': 'Georgia',
+            'GU': 'Guam',
+            'HI': 'Hawaii',
+            'IA': 'Iowa',
+            'ID': 'Idaho',
+            'IL': 'Illinois',
+            'IN': 'Indiana',
+            'KS': 'Kansas',
+            'KY': 'Kentucky',
+            'LA': 'Louisiana',
+            'MA': 'Massachusetts',
+            'MD': 'Maryland',
+            'ME': 'Maine',
+            'MI': 'Michigan',
+            'MN': 'Minnesota',
+            'MO': 'Missouri',
+            'MP': 'Northern Mariana Islands',
+            'MS': 'Mississippi',
+            'MT': 'Montana',
+            'NA': 'National',
+            'NC': 'North Carolina',
+            'ND': 'North Dakota',
+            'NE': 'Nebraska',
+            'NH': 'New Hampshire',
+            'NJ': 'New Jersey',
+            'NM': 'New Mexico',
+            'NV': 'Nevada',
+            'NY': 'New York',
+            'OH': 'Ohio',
+            'OK': 'Oklahoma',
+            'OR': 'Oregon',
+            'PA': 'Pennsylvania',
+            'PR': 'Puerto Rico',
+            'RI': 'Rhode Island',
+            'SC': 'South Carolina',
+            'SD': 'South Dakota',
+            'TN': 'Tennessee',
+            'TX': 'Texas',
+            'UT': 'Utah',
+            'VA': 'Virginia',
+            'VI': 'Virgin Islands',
+            'VT': 'Vermont',
+            'WA': 'Washington',
+            'WI': 'Wisconsin',
+            'WV': 'West Virginia',
+            'WY': 'Wyoming'
+    }
+
+    def abbrev_replace(abbrev):
+        return state_dict[abbrev]
+
+    def state_replace(state_name):
+        reverse_state_dict = dict((v, k) for k, v in state_dict.items()) 
+        return reverse_state_dict[state_name]
+
+    if abbr_2_st:
+        return state_series.apply(abbrev_replace)
+    else:
+        return state_series.apply(state_replace)
+    
+
+addy_states = addy_split(address_df['address'])['state']
+
+full_state_names_col = abbr_2_st(addy_states)
+
+
+# print(abbr_2_st(full_state_names_col, abbr_2_st=False))
+# # print(abbr_2_st(addy_states))
+
+
+
+
 
 def list_2_series(list_2_series, df):
-    pass
+    new_column = pd.Series(list_2_series)
+    return pd.concat([df, new_column], axis=1)
 
-def rm_doutlier(df):
-    pass
+# print(list_2_series([10, 11, 12], test_df))
+
+
+outlier_df = pd.DataFrame(
+    {'a': [1, 2, 3, 4, 5, 6],
+     'b': [4, 5, 6, 7, 8, 9],
+     'c': [7, 1000, 9, 10, 11, 12]}
+)
+
+def rm_outlier(df):
+    
+
+    cleaned_df = pd.DataFrame()
+
+    for (columnName, columnData) in df.iteritems():
+        Q1 = columnData.quantile(0.25)
+        Q3 = columnData.quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - (1.5*IQR)
+        upper_bound = Q3 + (1.5*IQR)
+        # print(lower_bound, upper_bound)
+
+        mask = columnData.between(lower_bound, upper_bound, inclusive='both')
+        cleaned = columnData.loc[mask]
+        
+        cleaned_df[columnName] = cleaned
+
+    return cleaned_df
+
+
+# print(rm_outlier(outlier_df))
+
 
 def split_dates(date_series):
-    pass
+    # mm/dd/yyyy
+    df = pd.DataFrame()
+
+    month_list = []
+    day_list = []
+    year_list = []
+    
+    for date in date_series:
+        month_list.append(date.split('/')[0])
+        day_list.append(date.split('/')[1])
+        year_list.append(date.split('/')[2])
+
+    df['month'] = month_list
+    df['day'] = day_list
+    df['year'] = year_list
+
+    return df
+
+
+
+
+print(split_dates(pd.Series(['01/13/2016', '02/14/2017', '03/15/2018', '04/16/2019'])))
